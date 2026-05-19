e2a52317ccbcb2255f6111f27cb547b5c0807a57
Skandar Redfield <skan.redfield@gmail.com>
2026-05-19 21:41:09 +0300

    Moved the force computation code to the BodySystem class -- canvas is now purely for drawing. Added a text file with goals to implement.

--- body_system.py
+++ body_system.py
@@ -1,10 +1,48 @@
+import numpy as np
+
+
 class BodySystem:
 
-    def __init__(self):
+    def __init__(self, grav_const):
         self.bodies = []
+        self.grav_const = grav_const
 
     def add_body(self, body):
         self.bodies.append(body)
         
     def remove_body(self, body):
         self.bodies.remove(body)
+
+    def evolve_to_time(self, frames):
+        tspace = np.linspace(0, frames, frames)
+        rspace = np.empty(frames)
+        thetaspace = np.empty(frames)
+
+        for i, t in enumerate(tspace):
+            for j, body1 in enumerate(self.bodies):
+                for k, body2 in enumerate(self.bodies):
+                    if j == k:
+                        continue
+
+                    mu = -self.grav_const * body1.mass * body2.mass
+
+                    dx = body2.x - body1.x
+                    dy = body2.y - body1.y
+                    dr = np.sqrt(dx**2 + dy**2)
+                    rspace[i] = dr
+                    force = mu / dr**2
+
+                    theta = np.atan2(dy, dx)
+                    thetaspace[i] = theta
+                    fx = np.cos(theta) * force
+                    fy = np.sin(theta) * force
+                    fvec = (fx, fy, 0)
+                    neg_fvec = (-fx, -fy, 0)
+
+                    dt = t / 1000.0
+
+                    # update the orbiter's parameters
+                    body2.update(dt, fvec)
+                    body1.update(dt, neg_fvec)
+
+        return (tspace, rspace, thetaspace)
--- sim_canvas.py
+++ sim_canvas.py
@@ -1,38 +1,21 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
+from body_system import BodySystem
 from massive_body import MassiveBody
 
 
 G = 1
 
+
+system = BodySystem(G)
+
 body1 = MassiveBody(1000.0, (0, 0, 0), (0, 0, 0))
 body2 = MassiveBody(1.0, (10, 0, 0), (0, 10, 0))
 
-mu = -G * body1.mass * body2.mass
+system.add_body(body1)
+system.add_body(body2)
 
-tspace = np.linspace(0, 500, 500)
-rspace = []
-thetaspace = []
-
-for t in tspace:
-    dx = body2.x - body1.x
-    dy = body2.y - body1.y
-    dr = np.sqrt(dx**2 + dy**2)
-    rspace.append(dr)
-    force = mu / dr**2
-
-    theta = np.atan2(dy, dx)
-    thetaspace.append(theta)
-    fx = np.cos(theta) * force
-    fy = np.sin(theta) * force
-    fvec = (fx, fy, 0)
-
-    dt = t / 1000.0
-
-    # update the orbiter's parameters
-    body2.update(dt, fvec)
-
+tspace, rspace, thetaspace = system.evolve_to_time(500)
 
 fig = plt.figure()
 
@@ -44,6 +27,7 @@ ax1.set_ylabel(r"$r$")
 
 ax2 = fig.add_subplot(1, 2, 2, projection='polar')
 ax2.plot(thetaspace, rspace)
+# ax2.set_ylim(min(rspace)-0.5, max(rspace)+0.5)
 ax2.set_title("Polar")
 ax2.set_xlabel(r"$\theta$")
 ax2.set_ylabel(r"$r$")
